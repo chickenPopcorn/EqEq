b051b4f5ee73a8fb29c4ffabbaaed086962c84db
Jonathan Zacsh <jzacsh@gmail.com>
2016-03-07 13:52:58 -0500

    last minute fixes to script to allow for hand-crafted touch-ups to LRM pdf via standard HTML webdev

--- notes/lrm-to-pdf.sh
+++ notes/lrm-to-pdf.sh
@@ -2,7 +2,7 @@
 #
 # Converts LRM doc to PDF including a table of contents.
 
-USE_PANDOC=1 # toggle this as `1` or `0` without quotes
+USE_PANDOC=0 # toggle this as `1` or `0` without quotes
 
 set -e
 declare -r lrmToPdfDir="$(mktemp --directory --tmpdir  "$(basename "$0")"_XXXXXX.d)"
@@ -37,19 +37,42 @@ markedExec="$(getExecPath marked node_modules/marked/bin/marked)"
 # <script>hljs.initHighlightingOnLoad();</script>
 cat > ./lrm.html <<-EOF_STYLES
 <style type="text/css">
-  @font-face {
-    font-family: LiberationSerif;
-    src: url('file:///usr/share/fonts/truetype/liberation/LiberationSerif-Regular.ttf');
-  }
-  body, p, h1, h2, h3, h4, h5, h6 { font-family: serif; }
-  body {
-    padding: 7em;
-    font-family: serif;
-    font-size: 21pt;
-  }
-  h1:nth-of-type(1) { text-align: center; }
+    body {
+      padding: 4em;
+      font-family: LiberationSerif, serif;
+      font-size: 12pt;
+    }
+    #toc {
+      margin: 1em auto 9em auto;
+    }
+    #toc a {
+      text-decoration: none;
+      color: inherit;
+    }
+    h1:nth-of-type(1) {
+      text-align: center;
+      margin: 3em 0 0 0;
+    }
+    a {
+      /** since we're not inserting foot-notes, this is just strange */
+      text-decoration: none;
+      color: inherit;
+    }
+    pre {
+      padding: 1em 0 1em 1ex;
+      background-color: #FFF4DB;
+    }
+    code { font-size:.7em; }
+    p { line-height: 1.25em; }
+    #authors table {
+      margin: 1em auto;
+      text-align: left;
+      width: 60%;
+    }
+    #authors table tbody { outline: 1px solid #bbb;}
 </style>
 <script>
+'use strict';
 window.onload = function() {
   var codes = document.querySelectorAll('code');
   for (let idx in codes) {
