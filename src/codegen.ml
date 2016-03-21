5f58c11a19c9156ac326e53d281d290d859c0219
Jonathan Zacsh <jzacsh@gmail.com>
2016-03-21 14:32:42 -0400

    GH-#8 dumb semant-analy, start codegen anyway...
    stopping work on semantic analysis, and going to create a godegen that prints
    straight `int main() { print(42); }`, so we can start parallelizing tasks (ie:
    have each of us work on one file,, and have a shared e2e test to run our code
    against).

--- src/codegen.ml
+++ src/codegen.ml
@@ -1,16 +1,4 @@
-(* Code generation: translate takes a semantically checked AST and
-produces LLVM IR
-
-LLVM tutorial: Make sure to read the OCaml version of the tutorial
-
-http://llvm.org/docs/tutorial/index.html
-
-Detailed documentation on the OCaml LLVM library:
-
-http://llvm.moe/
-http://llvm.moe/ocaml/
-
-*)
+(* Code generation: translates semantically checked AST & produces vanilla C. *)
 
 module L = Llvm
 module A = Ast
