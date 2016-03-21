b9fa88ef2d0d4ef441be759a82e0ccfc9ae13b4d
Tianci Zhong <tz2278@columbia.edu>
2016-03-21 19:19:55 -0400

    try

--- src/ast.ml
+++ src/ast.ml
@@ -1,4 +1,4 @@
-(* Abstract Syntax Tree and functions for printing it *)
+(* Abstract Syntax Tree and functions for printing it   :*)
 
 type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq |
           And | Or
