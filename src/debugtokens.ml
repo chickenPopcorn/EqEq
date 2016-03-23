f7e5d0a528f5f478b854d8da5eb81b56662d11ff
Jonathan Zacsh <jzacsh@gmail.com>
2016-03-23 13:05:33 -0400

    adds debugging helper to dump what our scanner interprets for agiven source proram

---
+++ src/debugtokens.ml
@@ -0,0 +1,46 @@
+open Parser
+open Ast
+
+type num =
+  | Num_int of int
+  | Num_float of float 
+
+let stringify = function
+  | LPAREN -> "LPAREN"
+  | RPAREN -> "RPAREN"
+  | LBRACE -> "LBRACE"
+  | RBRACE -> "RBRACE"
+  | SEMI -> "SEMI"
+  | COLON -> "COLON"
+  | COMMA -> "COMMA"
+  | PLUS -> "PLUS"
+  | MINUS -> "MINUS"
+  | TIMES -> "TIMES"
+  | DIVIDE -> "DIVIDE"
+  | ASSIGN -> "ASSIGN"
+  | EQ -> "EQ"
+  | NEQ -> "NEQ"
+  | LT -> "LT"
+  | LEQ -> "LEQ"
+  | GT -> "GT"
+  | GEQ -> "GEQ"
+  | AND -> "AND"
+  | OR -> "OR"
+  | NOT -> "NOT"
+  | IF -> "IF"
+  | ELSE -> "ELSE"
+  | WHILE -> "WHILE"
+  | FIND -> "FIND"
+  | LITERAL(lxm) -> "LITERAL"
+  | ID(lxm) -> "ID"
+  | CTX(lxm) -> "CTX"
+  | EOF -> "EOF"
+
+let _ = 
+  let lexbuf = Lexing.from_channel stdin in
+  let rec print_tokens = function
+    | EOF -> " "
+    | token ->
+      print_endline (stringify token);
+      print_tokens (Scanner.token lexbuf) in
+  print_tokens (Scanner.token lexbuf)
