5e08177f08046bdb6432006eac60babb897138ac
Ruicong Xie <rx2119@columbia.edu>
2016-04-06 21:09:49 -0400

    codegen using if pretty print which is not implemented correctly for all subsequent expressions

--- src/codegen.ml
+++ src/codegen.ml
@@ -22,14 +22,15 @@ let translate (contexts, finds, varmap) =
     | A.Builtin("print", el) -> "printf(" ^ String.concat ", " (List.map gen_expr el) ^ ")"
     | A.Builtin(f, el) -> f ^ "(" ^ String.concat ", " (List.map gen_expr el) ^ ")"
   in
+
   let rec gen_stmt = function
     | A.Block(stmts) ->
         "{\n" ^ String.concat "" (List.map gen_stmt stmts) ^ "}\n"
     | A.Expr(expr) -> gen_expr expr ^ ";\n";
-    | A.If(e, s, Block([])) -> "if (" ^ gen_expr e ^ ")\n" ^ gen_stmt s
-    | A.If(e, s1, s2) ->  "if (" ^ gen_expr e ^ ")\n" ^
-        gen_stmt s1 ^ "else\n" ^ gen_stmt s2
     | A.While(e, s) -> "while (" ^ gen_expr e ^ ") " ^ gen_stmt s
+    | A.If (l) ->  A.string_of_first_cond_exec (List.hd l) ^ "\n" ^
+    (String.concat "\n" (List.map A.string_of_cond_exec (List.tl l)))
+
   in
   let gen_decl_var varname funcdecl str =
     "double " ^ varname ^ ";\n" ^ str
@@ -46,25 +47,25 @@ let translate (contexts, finds, varmap) =
   let gen_ctxdecl ctx =
     String.concat "" (List.map gen_multieq ctx.A.cbody)
   in
-  let gen_finddecl finddecl = 
+  let gen_finddecl finddecl =
     String.concat "" (List.map gen_stmt finddecl.A.fbody)
   in
-  let gen_find_funcname_list finddecl_list = 
-    let rec gen_find_funcname count find_list = 
+  let gen_find_funcname_list finddecl_list =
+    let rec gen_find_funcname count find_list =
       match find_list with
       | [] -> []
       | hd::tl -> ("find_" ^ hd.A.fcontext ^ "_" ^ (string_of_int count))::(gen_find_funcname (count+1) tl)
     in List.rev (gen_find_funcname 0 finddecl_list)
-  in 
-  let gen_find_function find_funcname finddecl = 
+  in
+  let gen_find_function find_funcname finddecl =
     (* naming of the function: find_(context_name)_(golabl_counting_num) *)
-    "void " ^ find_funcname ^ " () {\n  " ^ 
-    String.concat ""(List.map gen_decl_ctx contexts) ^ 
+    "void " ^ find_funcname ^ " () {\n  " ^
+    String.concat ""(List.map gen_decl_ctx contexts) ^
     String.concat ""(List.map gen_ctxdecl contexts) ^
     (gen_finddecl finddecl) ^ "}\n" ^
     "\n"
   in
-  let gen_findfunc_call find_funcname finddecl = 
+  let gen_findfunc_call find_funcname finddecl =
     find_funcname ^ " ();\n"
   in
 
