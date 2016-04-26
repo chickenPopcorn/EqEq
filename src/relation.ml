69cbb84162f6d2ff8d7672a841ed9dab820c6417
Jonathan Zacsh <jzacsh@gmail.com>
2016-04-25 21:04:12 -0400

    noop: move fold_left funnctions to `relation.ml`
    Hopefully addresses complaint that code is too large. Is definitely clearer how
    the new part of the sast is being onstructed, now.

---
+++ src/relation.ml
@@ -0,0 +1,147 @@
+module A = Ast
+module S = Sast
+module StringMap = Map.Make(String)
+
+(* Mapper for List.fold_left to accumulate an initial map of contexts'
+ * equations, before, and start an empty map for their find blocks. *)
+let relationCtxFolder (relations : S.eqResolutions) ctx =
+  (* internal helper *)
+  let rec findDepsInAssignStmt foundDeps stmt =
+    let rec findIdsInExpr foundIds = function
+      | A.Literal(_) -> foundIds
+      | A.Id(id) -> id::foundIds
+      | A.Strlit(_) -> foundIds
+      | A.Binop(e1,_,e2) -> findIdsInExpr (findIdsInExpr foundIds e2) e1
+      | A.Unop(_,e) -> findIdsInExpr foundIds e
+      | A.Assign(_,e) -> findIdsInExpr foundIds e
+      | A.Builtin(_,eList) ->
+        List.fold_left (fun ls e -> findIdsInExpr ls e) foundIds eList
+    in
+
+    match stmt with
+    | A.Block(sList) ->
+      List.fold_left (fun ls s -> findDepsInAssignStmt ls s) foundDeps sList
+    | A.Expr(e) -> findIdsInExpr foundDeps e
+    | A.If(stmtOrTupleList) -> (
+        let rec idsInIf accumul = function
+          | [] -> accumul
+          | (None,s)::tail -> idsInIf (findDepsInAssignStmt accumul s) tail
+          | (Some(e),s)::tail ->
+            idsInIf (findIdsInExpr (findDepsInAssignStmt accumul s) e) tail
+        in idsInIf foundDeps stmtOrTupleList
+      )
+    | A.While(e, s) -> findIdsInExpr (findDepsInAssignStmt foundDeps s) e
+  in
+
+  let ctxScope =
+    let (deps, indeps) =
+      let ctx_body_folder (deps, indeps) mEq =
+        let multi_eq_folder (deps, indeps) mEqBody =
+          let foundDeps = findDepsInAssignStmt [] mEqBody in
+          if List.length foundDeps > 0
+          then (StringMap.add mEq.A.fname foundDeps deps, indeps)
+          else (deps, StringMap.add mEq.A.fname mEq.A.fdbody indeps)
+
+        in List.fold_left multi_eq_folder (deps, indeps) mEq.A.fdbody
+
+      in List.fold_left
+        ctx_body_folder
+        (StringMap.empty, StringMap.empty)
+        ctx.A.cbody
+
+    in {
+      S.ctx_deps = deps;
+      S.ctx_indeps = indeps;
+
+      (* taken care of in `relationFindFolder` later on ... *)
+      S.ctx_finds = StringMap.empty;
+    }
+  in StringMap.add ctx.A.context ctxScope relations
+
+let rec findStmtRelator (m, i) (st : A.stmt) =
+  let fail msg = raise (Failure msg) in
+  let quot content = "\"" ^ content ^  "\"" in
+
+  let rec findExprRelator (eMap, idx) (expr : A.expr) =
+    (* Oldest value in `map` no greater than key `i` *)
+    let oldest (asof : int) (m : S.equation_relations S.IntMap.t) =
+      let rec walkBack i =
+        try S.IntMap.find i m with Not_found ->
+          if i > 0 then walkBack (i - 1) else fail (
+              Printf.sprintf
+                "Compiler BUG found: empty rel-map at expression #%d [only found: '%s']"
+                asof (String.concat "', '" (
+                    S.IntMap.fold
+                      (fun k v a -> (string_of_int k)::a)
+                      m []
+                  ))
+            )
+      in walkBack asof
+    in
+
+    let check_resolvable (index : int) (id : string) m =
+      let assert_nodeps id rels = try StringMap.find id rels with
+        | Not_found -> fail ("Unresolvable identifier, " ^ quot id)
+
+      (* TODO NEXT STEP: BFS on deps/indeps to give real answer *)
+
+      in assert_nodeps id (oldest index m).S.indeps
+    in
+
+    let i = idx + 1 in match expr with
+    | A.Id(_) | A.Literal(_) | A.Strlit(_) -> (eMap, i)
+    | A.Binop(eLeft, _, eRight) ->
+      findExprRelator (findExprRelator (eMap, i) eLeft) eRight
+    | A.Unop(_, e) -> findExprRelator (eMap, i) e
+    | A.Assign(id, e) ->
+      (* TODO: figure out how to ensure failures for
+       * `undefinedvar` in `e` for an expression:
+       *     `find{ a = b = undefinedvar + 1}`
+      *)
+      let rec chk_right_indep = function
+        | A.Id(id) -> ignore (check_resolvable i id eMap);
+        | A.Literal(_) | A.Strlit(_) -> ignore ();
+        | A.Binop(left, _, right) ->
+          ignore (List.iter chk_right_indep [left; right]);
+        | A.Unop(_, e) -> ignore (chk_right_indep e);
+        | A.Assign(_, e) -> ignore (chk_right_indep e);
+        | A.Builtin(_, eLi) -> ignore (List.iter chk_right_indep eLi);
+      in ignore (chk_right_indep);
+
+      let latest = oldest i eMap in
+
+      (* If `id` already exists, then it's being redefined, in
+       * which case we'll start a new S.equation_relations the
+       * current expression index. Else we'll keep using the
+       * current S.equation_relations, `eMap` as-is.
+      *)
+      let maybeExtendedExprMap =
+        let isKnownEquation =
+          StringMap.mem id latest.S.indeps ||
+          StringMap.mem id latest.S.deps
+        in
+
+        if isKnownEquation
+        then S.IntMap.add i latest eMap
+        else eMap
+
+      in findExprRelator (maybeExtendedExprMap, i) e
+    | A.Builtin(_, exprLis) ->
+      List.fold_left findExprRelator (eMap, i) exprLis
+
+  in match st with
+  | A.Block(s) -> List.fold_left findStmtRelator (m, i) s
+  | A.Expr(e) -> findExprRelator (m, i) e
+  | A.If(stmtTupleWithOptionalExpr) ->
+    let rec relationsInIf accum = function
+      | [] -> accum
+      | (None, s)::tail ->
+        relationsInIf (findStmtRelator accum s) tail
+      | (Some(e), s)::tail ->
+        relationsInIf (
+          findStmtRelator (
+            findExprRelator accum e
+          ) s
+        ) tail
+    in relationsInIf (m, i) stmtTupleWithOptionalExpr
+  | A.While(e, s) -> findStmtRelator (findExprRelator (m, i) e) s
--- src/semant.ml
+++ src/semant.ml
@@ -2,21 +2,16 @@
 
 module A = Ast
 module S = Sast
+module R = Relation
 module StringMap = Map.Make(String)
 
 (* Semantic checking of a program. Returns void if successful,
    throws an exception if something is wrong.
 
    Check each context, then check each find declaration *)
-
 let check (contexts, finds) =
   let fail msg = raise (Failure msg) in
-
-  (* string prettifiers *)
   let quot content = "\"" ^ content ^  "\"" in
-  let ex_qt expr = quot (A.string_of_expr expr) in
-  let bop_qt bop = quot (A.string_of_op bop) in
-  let uop_qt uop = quot (A.string_of_uop uop) in
 
   (* Raise an exception of the given rvalue type cannot be assigned to
      the given lvalue type
@@ -27,67 +22,9 @@ let check (contexts, finds) =
   TODO: possible ^ given how we've structured string-literals in our grammar? *)
 
   let eqrelations : S.eqResolutions =
-    let rec findDepsInAssignStmt foundDeps stmt =
-      let rec findIdsInExpr foundIds = function
-        | A.Literal(_) -> foundIds
-        | A.Id(id) -> id::foundIds
-        | A.Strlit(_) -> foundIds
-        | A.Binop(e1,_,e2) -> findIdsInExpr (findIdsInExpr foundIds e2) e1
-        | A.Unop(_,e) -> findIdsInExpr foundIds e
-        | A.Assign(_,e) -> findIdsInExpr foundIds e
-        | A.Builtin(_,eList) ->
-            List.fold_left (fun ls e -> findIdsInExpr ls e) foundIds eList
-      in
-
-      match stmt with
-        | A.Block(sList) ->
-            List.fold_left
-              (fun ls s -> findDepsInAssignStmt ls s)
-              foundDeps
-              sList
-        | A.Expr(e) -> findIdsInExpr foundDeps e
-        | A.If(stmtOrTupleList) -> (
-            let rec collectIdsInIf accumul = function
-              | [] -> accumul
-              | (None,s)::tail ->
-                  collectIdsInIf (findDepsInAssignStmt accumul s) tail
-              | (Some(e),s)::tail ->
-                  collectIdsInIf (
-                    findIdsInExpr (findDepsInAssignStmt accumul s) e
-                  ) tail
-            in collectIdsInIf foundDeps stmtOrTupleList
-          )
-        | A.While(e, s) -> findIdsInExpr (findDepsInAssignStmt foundDeps s) e
-    in
-
     (* `Sast.eqResolutions` to which we'll add `S.equation_relations` *)
     let ctxRelations : S.eqResolutions =
-      let relationCtxFolder relations ctx =
-        let ctxScope =
-          let (deps, indeps) =
-            let ctx_body_folder (deps, indeps) mEq =
-              let multi_eq_folder (deps, indeps) mEqBody =
-                let foundDeps = findDepsInAssignStmt [] mEqBody in
-                if List.length foundDeps > 0
-                then (StringMap.add mEq.A.fname foundDeps deps, indeps)
-                else (deps, StringMap.add mEq.A.fname mEq.A.fdbody indeps)
-              in
-              List.fold_left multi_eq_folder (deps, indeps) mEq.A.fdbody
-
-            in
-            List.fold_left ctx_body_folder (StringMap.empty, StringMap.empty) ctx.A.cbody
-          in
-          {
-            S.ctx_deps = deps;
-            S.ctx_indeps = indeps;
-
-            (* taken care of in `relationFindFolder` later on ... *)
-            S.ctx_finds = StringMap.empty;
-          }
-        in
-        StringMap.add ctx.A.context ctxScope relations
-      in
-      List.fold_left relationCtxFolder StringMap.empty contexts
+      List.fold_left R.relationCtxFolder StringMap.empty contexts
     in
 
     (* Add a complete picture of contexts' find decl relations, maintaining an
@@ -95,18 +32,18 @@ let check (contexts, finds) =
      * completed map. *)
     let (sastEqRels, _) =
       (* Fold `S.equation_relations` to respective Contexts' `S.ctx_scopes` *)
-      let relationFindFolder (relations, findIdx) findDec =
-        let findName = Printf.sprintf "find_%s_%d" findDec.A.fcontext findIdx in
+      let relationFindFolder (relations, findIdx) fnDec =
+        let findName = Printf.sprintf "find_%s_%d" fnDec.A.fcontext findIdx in
 
         (* `Sast.ctx_scopes` for which we're creating an `findName` entry. *)
         let ctxScopes : Sast.ctx_scopes =
-          try StringMap.find findDec.A.fcontext relations
+          try StringMap.find fnDec.A.fcontext relations
           with Not_found ->
-            fail ("find targeting unknown context, " ^ quot findDec.A.fcontext)
+            fail ("find targeting unknown context, " ^ quot fnDec.A.fcontext)
         in
 
         (* TODO: after building deps/indeps (findBodyMap), ensure
-         * `findDec.ftarget` is known key (of either deps or indeps is fine) *)
+         * `fnDec.ftarget` is known key (of either deps or indeps is fine) *)
 
         let extendedRels : S.eqResolutions =
           (* Map from expression index to a `Sast.equation_relations` *)
@@ -114,92 +51,6 @@ let check (contexts, finds) =
             (* Build a complete map of expresion-index to relations for this
              * find body, then discard the latest index and return that map. *)
             let (eqRels, _) =
-              let rec findExprRelator (eMap, idx) (expr : A.expr) =
-                (* Oldest value in `map` no greater than key `i` *)
-                let oldest (asof : int) (m : S.equation_relations S.IntMap.t) =
-                  let rec walkBack i =
-                    try S.IntMap.find i m with Not_found ->
-                      if i > 0 then walkBack (i - 1) else fail (
-                        Printf.sprintf
-                          "Compiler BUG found: empty rel-map at expression #%d [only found: '%s']"
-                          asof (String.concat "', '" (
-                            S.IntMap.fold
-                              (fun k v a -> (string_of_int k)::a)
-                              m []
-                          ))
-                      )
-                  in walkBack asof
-                in
-
-                let check_resolvable (index : int) (id : string) m =
-                  let assert_nodeps id rels = try StringMap.find id rels with
-                    | Not_found -> fail ("Unresolvable identifier, " ^ quot id)
-
-                    (* TODO NEXT STEP: BFS on deps/indeps to give real answer *)
-
-                  in assert_nodeps id (oldest index m).S.indeps
-                in
-
-                let i = idx + 1 in match expr with
-                  | A.Id(_) | A.Literal(_) | A.Strlit(_) -> (eMap, i)
-                  | A.Binop(eLeft, _, eRight) ->
-                      findExprRelator (findExprRelator (eMap, i) eLeft) eRight
-                  | A.Unop(_, e) -> findExprRelator (eMap, i) e
-                  | A.Assign(id, e) ->
-                      (* TODO: figure out how to ensure failures for
-                       * `undefinedvar` in `e` for an expression:
-                       *     `find{ a = b = undefinedvar + 1}`
-                       *)
-                      let rec chk_right_indep = function
-                        | A.Id(id) -> ignore (check_resolvable i id eMap);
-                        | A.Literal(_) | A.Strlit(_) -> ignore ();
-                        | A.Binop(left, _, right) ->
-                            ignore (List.iter chk_right_indep [left; right]);
-                        | A.Unop(_, e) -> ignore (chk_right_indep e);
-                        | A.Assign(_, e) -> ignore (chk_right_indep e);
-                        | A.Builtin(_, eLi) -> ignore (List.iter chk_right_indep eLi);
-                      in ignore (chk_right_indep);
-
-                      let latest = oldest i eMap in
-
-                      (* If `id` already exists, then it's being redefined, in
-                       * which case we'll start a new S.equation_relations the
-                       * current expression index. Else we'll keep using the
-                       * current S.equation_relations, `eMap` as-is.
-                       *)
-                      let maybeExtendedExprMap =
-                        let isKnownEquation =
-                          StringMap.mem id latest.S.indeps ||
-                          StringMap.mem id latest.S.deps
-                        in
-
-                        if isKnownEquation
-                        then S.IntMap.add i latest eMap
-                        else eMap
-
-                      in findExprRelator (maybeExtendedExprMap, i) e
-                  | A.Builtin(_, exprLis) ->
-                      List.fold_left findExprRelator (eMap, i) exprLis
-              in
-
-              let rec findStmtRelator (m, i) (st : A.stmt) = match st with
-                | A.Block(s) -> List.fold_left findStmtRelator (m, i) s
-                | A.Expr(e) -> findExprRelator (m, i) e
-                | A.If(stmtTupleWithOptionalExpr) ->
-                    let rec relationsInIf accum = function
-                      | [] -> accum
-                      | (None, s)::tail ->
-                          relationsInIf (findStmtRelator accum s) tail
-                      | (Some(e), s)::tail ->
-                          relationsInIf (
-                            findStmtRelator (
-                              findExprRelator accum e
-                            ) s
-                          ) tail
-                    in relationsInIf (m, i) stmtTupleWithOptionalExpr
-                | A.While(e, s) -> findStmtRelator (findExprRelator (m, i) e) s
-              in
-
               (* Initial map from starting with contexts' own relationships *)
               let exprMap : S.equation_relations S.IntMap.t =
                 let baseRelations : S.equation_relations = {
@@ -209,9 +60,8 @@ let check (contexts, finds) =
                 in
                 S.IntMap.add 0 baseRelations S.IntMap.empty
               in
-              List.fold_left findStmtRelator (exprMap, 0) findDec.A.fbody
-            in
-            eqRels
+              List.fold_left R.findStmtRelator (exprMap, 0) fnDec.A.fbody
+            in eqRels
           in
 
           let ctxFinds : S.find_scopes =
@@ -223,14 +73,13 @@ let check (contexts, finds) =
             S.ctx_indeps = ctxScopes.S.ctx_indeps;
             S.ctx_finds = ctxFinds;
           }
-          in
-          StringMap.add findDec.A.fcontext scopes relations
-        in
-        (extendedRels, findIdx + 1)
-      in
-      List.fold_left relationFindFolder (ctxRelations, 0) finds
-    in
-    sastEqRels
+          in StringMap.add fnDec.A.fcontext scopes relations
+
+        in (extendedRels, findIdx + 1)
+
+      in List.fold_left relationFindFolder (ctxRelations, 0) finds
+
+    in sastEqRels
   in
 
   (* Map of variables to their decls. For more, see: S.varMap *)
